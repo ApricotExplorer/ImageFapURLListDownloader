1a7081698a68ab4b50d2e74d2cc4dc6385d3787c
ApricotExplorer <apricotexplorer11@gmail.com>
2022-04-04 23:20:36 +0100

    updated lists and file structure

--- downloadUrls.sh
+++ downloadUrls.sh
@@ -1,7 +1,6 @@
-echo "test"
 while read name
 do
-	python3 /home/mattioiscattio/Videos/GalleryDownloader/IFDownloader.py
+	python3 IFDownloader.py
     "$name"
-done < urls.txt
+done < urlList.txt
  
